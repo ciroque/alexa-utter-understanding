9d71db088b5f0ca4f64eace0c530d93ecb1e6e7b
Steve Wagner <swagner@swagner-mac.local>
2016-10-31 08:07:11 -0700

    Adding logging.

--- src/main/handlers/intent/AmazonHelpIntentHandler.ts
+++ src/main/handlers/intent/AmazonHelpIntentHandler.ts
@@ -1,9 +1,12 @@
 import IRequestHandler from '../IRequestHandler';
 import {AlexaResponse} from '../../response/AlexaResponse';
 import {AmznHelpIntentResponse} from '../../response/predefined/AmznHelpIntentResponse';
+import {Logger} from '../../Logger';
 
 export class AmazonHelpIntentHandler implements IRequestHandler {
+    private logger = new Logger('AmazonHelpIntentHandler');
     handleRequest(event: any, context: any): Promise<AlexaResponse> {
+        this.logger.debug('handling help intent');
         return new Promise((resolve: any) => { resolve(AmznHelpIntentResponse.getInstance({}, true)); });
     }
 }
--- src/main/handlers/intent/UnknownIntentHandler.ts
+++ src/main/handlers/intent/UnknownIntentHandler.ts
@@ -1,9 +1,12 @@
 import IRequestHandler from '../IRequestHandler';
 import {AlexaResponse} from '../../response/AlexaResponse';
 import {UnknownIntentResponse} from '../../response/predefined/UnknownIntentResponse';
+import {Logger} from '../../Logger';
 
 export class UnknownIntentHandler implements IRequestHandler {
+    private logger = new Logger('UnknownIntentHandler');
     handleRequest(event: any, context: any): Promise<AlexaResponse> {
+        this.logger.debug('handling unknown intent');
         return new Promise((resolve: any) => { resolve(UnknownIntentResponse.getInstance({}, true)); });
     }
 }
